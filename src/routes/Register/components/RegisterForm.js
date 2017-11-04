bf10454e715e2941db6463becb88301675114d6f
RomainRui <romain.rui10@gmail.com>
2017-11-04 22:50:21 +0100

    First commit

---
+++ src/routes/Register/components/RegisterForm.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Button, FormGroup, Form, ControlLabel, FormControl } from 'react-bootstrap'
+
+const FieldGroup = ({ id, label, help, ...props }) => {
+  return (
+    <FormGroup controlId={id}>
+      <ControlLabel>{label}</ControlLabel>
+      <FormControl {...props} />
+      {help && <HelpBlock>{help}</HelpBlock>}
+    </FormGroup>
+  );
+}
+
+class Register extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <form>
+          <FieldGroup
+            id="formControlsText"
+            type="text"
+            label="Text"
+            placeholder="Enter username"
+          />
+          <FieldGroup
+            id="formControlsEmail"
+            type="email"
+            label="Email address"
+            placeholder="Enter email"
+          />
+          <FieldGroup
+            id="formControlsPassword"
+            label="Password"
+            type="password"
+          />
+          <Button type="submit">
+            Submit
+          </Button>
+        </form>|
+      </div>
+    )
+  }
+}
+
+Register.propTypes = {
+  // counter: PropTypes.number.isRequired,
+  // increment: PropTypes.func.isRequired,
+  // doubleAsync: PropTypes.func.isRequired,
+}
+
+export default Register
